6b2ef32032e955881a7537cebdab8f2f921c5081
engooh <erickngooh@gmail.com>
2022-06-09 03:08:19 +0200

    philo bonus: pthread_check_death est stable ou casi-stable

--- src_bonus/kill_bonus.c
+++ src_bonus/kill_bonus.c
@@ -6,7 +6,7 @@
 /*   By: engooh <engooh@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 11:52:55 by engooh            #+#    #+#             */
-/*   Updated: 2022/06/09 01:56:39 by engooh           ###   ########.fr       */
+/*   Updated: 2022/06/09 03:06:32 by engooh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../inc/philo_bonus.h"
@@ -16,8 +16,6 @@ int	kill_process(t_philo *philo, int i)
 	int	status;
 
 	waitpid(-1, &status, 0);
-	if (WIFEXITED(status))
-		status = WEXITSTATUS(status);
 	i = -1;
 	while (++i < philo->philo_number)
 		kill(philo->pid[i], SIGKILL);
